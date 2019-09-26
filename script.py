747d5f44a98d95437f21cb444c2e6b65cd86035f
ezragoss <ezragoss@me.com>
2019-09-25 18:09:48 -0700

    aplay

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 # Script to read audio files from a folder and then choose one at random to play upon signal
 
 from os import walk
+from os import system
 from random import choice
 
 audio = [] # List of audio file names from the audio directory
@@ -8,8 +9,7 @@ for (dirpath, dirnames, filenames) in walk('./Audio/'): # Read in names from the
     audio.extend(filenames) # Populating the list
     break # Only need top level names for now
 
-for i in range(0,4):
-    print(choice(audio))
+system("aplay ./Audio/"+choice(audio))
 
 
 # Need an auto 
